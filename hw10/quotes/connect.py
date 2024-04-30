6e04438068f04ef2350db9e858a31997e0acd4f8
Gennadiy Shpak <gena_shpak@ukr.net>
2024-04-30 22:28:30 +0300

    Comm_30.04.24_22.28

--- hw10/quotes/connect.py
+++ hw10/quotes/connect.py
@@ -1,16 +1,6 @@
 from mongoengine import connect
 from hw10.settings import config
 
-# import configparser
-
-
-# config = configparser.ConfigParser()
-# config.read("quotes\config.ini")
-
-# mongo_user = config.get("DB", "user")
-# mongodb_pass = config.get("DB", "pass")
-# db_name = config.get("DB", "db_name")
-# domain = config.get("DB", "domain")
 
 mongo_user = config.MONGO_USER
 mongodb_pass = config.MONGO_PASS
